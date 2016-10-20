20cd4778fb1812da651bc00008824c1a6dcb41f7
Maia Stone <maiadov@gmail.com>
2016-10-20 16:10:04 -0600

    MessageBox separate component

---
+++ lib/components/MessageBox.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+class MessageBox extends React.Component {
+  constructor(){
+    super();
+  }
+
+  render() {
+
+  return (
+
+      <ul>
+        { this.props.messages.map(m => <li key={m.key}>{m.user.displayName}: {m.content}</li>) }
+
+      </ul>
+
+      );
+    }
+  }
+
+module.exports = MessageBox;
