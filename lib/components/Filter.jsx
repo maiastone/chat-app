d1be6763d5c72941c246aeed262db8a73feb5db8
Maia Stone <maiadov@gmail.com>
2016-10-22 12:42:34 -0600

    WIP filter

--- lib/components/Filter.jsx
+++ lib/components/Filter.jsx
@@ -6,7 +6,17 @@ class Filter extends React.Component {
     super();
   }
 
-
+// TODO filter through array and show or hide based on searchString
+    filterMessages(searchString) {
+    for (var i=0; i < this.array.length; i++) {
+    var existingMessage = this.array[i];
+    if (!(this.array[i].input.includes(searchString))) {
+      array.children(this.array[i]).hide();
+    } else {
+      array.children(this.array[i]).show();
+    }
+  }
+}
 
 
   render() {
