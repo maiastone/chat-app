82a4e2b62d2c622682ae0cf4712e8fb36572e92f
Maia Stone <maiadov@gmail.com>
2016-10-25 17:58:51 -0600

    delete commented out code

--- lib/components/MessageBox.jsx
+++ lib/components/MessageBox.jsx
@@ -6,26 +6,6 @@ class MessageBox extends React.Component {
   constructor(){
     super();
   }
-// userArray ( this.props.filteredArray ) ? this.props.filteredArray : this.props.messages;
-// currentArray (this.props.filteredUserArray) ? (this.props.filteredUserArray) : this.props.messages;
-// function to determine which array to render first??
-// if filteredUser then currentArray = filteredUserArray
-// else current Array = filteredTextArray
-// else current Array = this.props.messages
-//
-//   decideArray() {
-//     var currentArray = this.props.messages;
-//     return (this.props.filteredUserArray)
-// //     currentArray = (this.props.filteredUserArray);
-// //   } else if ( this.props.filteredArray ) {
-// //     currentArray = (this.props.filteredArray);
-// //   }  else  {
-// //     currentArray =
-// //   }
-// //     return currentArray;
-// // // }
-// filteredArray={filteredArray}
-// filteredUserArray={filteredUserArray}
 
 
   render() {
